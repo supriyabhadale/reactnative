5afabeffddee6e1a16bcac07df9937ebcaa944af
supriyabhadale <supriya.bhadale18@gmail.com>
2019-09-20 19:56:20 +0530

    upload register

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+ 
+//Define your host here.
+$HostName = "127.0.0.1";
+ 
+//Define your database name here.
+$DatabaseName = "users";
+ 
+//Define your database username here.
+$HostUser = "root";
+ 
+//Define your database password here.
+$HostPass = "";
+ 
+?>
